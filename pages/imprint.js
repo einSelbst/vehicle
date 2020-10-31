b017ca615b8f987d273888ef28a9ef5e23b892af
Silvio <silvio@mailbox.org>
2020-10-31 22:07:20 +0100

    use all examples

---
+++ pages/imprint.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Imprint = () => (
+    <main>
+        <h1>Imprint</h1>
+    </main>
+);
+
+export default Imprint;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,34 @@
+import React from 'react'
 import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <div>
-      Hello World.{' '}
-      <ul>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/ssr">
-            <a>SSR</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/ssg">
-            <a>SSG</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  )
+const Home = () => {
+    const [state, setState] = React.useState(0)
+    return (
+        <main>
+            <h1>Hello from Preact</h1>
+            <p>{state}</p>
+            <button onClick={() => setState(state + 1)}>Increment</button>
+            <Link href="/about">
+                <a>About</a>
+            </Link>
+            <ul>
+                <li>
+                    <Link href="/imprint">
+                        <a>Imprint</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/ssr">
+                        <a>SSR</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/ssg">
+                        <a>SSG</a>
+                    </Link>
+                </li>
+            </ul>
+        </main>
+    )
 }
+export default Home;
