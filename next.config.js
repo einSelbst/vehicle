b80d50b7f81ce3b0b4a11fb82c980545a7767af6
Silvio <silvio@mailbox.org>
2020-11-01 01:32:35 +0100

    add some more sentry config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,22 @@ const preact = require('preact');
 const withPreact = require('next-plugin-preact');
 
 module.exports = withPreact({
-  experimental: {
-    plugins: true,
-    modern: true,
-  },
+    experimental: {
+        plugins: true,
+        modern: true,
+    },
+    
+    // Sentry.init config for server-side code. Can accept any available config option.
+    serverRuntimeConfig: {
+        sentry: {
+            type: 'server',
+        },
+    },
+    // Sentry.init config for client-side code (and fallback for server-side)
+    // can accept only serializeable values. For more granular control see below.
+    publicRuntimeConfig: {
+        sentry: {
+            type: 'client',
+        },
+    },
 });
