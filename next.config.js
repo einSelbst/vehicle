555ee08e57725d040e4d980e77a5b6aa064cc687
Silvio <silvio@mailbox.org>
2020-11-04 02:40:49 +0100

    use another config style

--- next.config.js
+++ next.config.js
@@ -28,92 +28,94 @@ const COMMIT_SHA =
 process.env.SENTRY_DSN = SENTRY_DSN
 const basePath = ''
 
-module.exports = withPlugins(
-    [
-        // Deploy source maps in production
-        [withSourceMaps],
-        [withPreact]
-    ],
-    {
-        experimental: {
-            plugins: true,
-            modern: true,
-        },
-        
-        // Sentry.init config for server-side code. Can accept any available config option.
-        serverRuntimeConfig: {
-            rootDir: __dirname,
-            sentry: {
-                type: 'server',
-            },
-        },
-        // Sentry.init config for client-side code (and fallback for server-side)
-        // can accept only serializeable values. For more granular control see below.
-        publicRuntimeConfig: {
-            sentry: {
-                type: 'client',
-            },
-        },
-        /* target: 'serverless', */
-        /* env: { */
-        /* SPACE_ID: process.env.CONTENTFUL_SPACE_ID, */
-        /* ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN, */
-        /* }, */
 
+const nextConfig = {
+    // distDir: 'build',
+    experimental: {
+        plugins: true,
+        modern: true,
+    },
+    
+    // Sentry.init config for server-side code. Can accept any available config option.
+    serverRuntimeConfig: {
+        rootDir: __dirname,
+        sentry: {
+            type: 'server',
+        },
+    },
+    // Sentry.init config for client-side code (and fallback for server-side)
+    // can accept only serializeable values. For more granular control see below.
+    publicRuntimeConfig: {
+        sentry: {
+            type: 'client',
+        },
+    },
+    /* target: 'serverless', */
+    /* env: { */
+    /* SPACE_ID: process.env.CONTENTFUL_SPACE_ID, */
+    /* ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN, */
+    /* }, */
+
+    // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+    webpack: (config, options) => {
+    // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Note: we provide webpack above so you should not `require` it
+        // Perform customizations to webpack config
+        /* config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//)) */
+        config.plugins.push(new BundleAnalyzerPlugin({  }))
+        /* config.plugins.push(new BundleAnalyzerPlugin({ token: process.env.BUNDLE_ANALYZER_TOKEN })) */
+
+
+        // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
+        // @sentry/node will run in a Node.js environment. @sentry/node will use
+        // Node.js-only APIs to catch even more unhandled exceptions.
+        //
+        // This works well when Next.js is SSRing your page on a server with
+        // Node.js, but it is not what we want when your client-side bundle is being
+        // executed by a browser.
+        //
+        // Luckily, Next.js will call this webpack function twice, once for the
+        // server and once for the client. Read more:
         // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-        // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        webpack: (config, options) => {
-            // Note: we provide webpack above so you should not `require` it
-            // Perform customizations to webpack config
-            config.plugins.push(new BundleAnalyzerPlugin({  }))
+        //
+        // So ask Webpack to replace @sentry/node imports with @sentry/browser when
+        // building the browser's bundle
+        if (!options.isServer) {
+            config.resolve.alias['@sentry/node'] = '@sentry/browser'
+        }
 
-            // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
-            // @sentry/node will run in a Node.js environment. @sentry/node will use
-            // Node.js-only APIs to catch even more unhandled exceptions.
-            //
-            // This works well when Next.js is SSRing your page on a server with
-            // Node.js, but it is not what we want when your client-side bundle is being
-            // executed by a browser.
-            //
-            // Luckily, Next.js will call this webpack function twice, once for the
-            // server and once for the client. Read more:
-            // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-            //
-            // So ask Webpack to replace @sentry/node imports with @sentry/browser when
-            // building the browser's bundle
-            if (!options.isServer) {
-                config.resolve.alias['@sentry/node'] = '@sentry/browser'
-            }
+        // When all the Sentry configuration env variables are available/configured
+        // The Sentry webpack plugin gets pushed to the webpack plugins to build
+        // and upload the source maps to sentry.
+        // This is an alternative to manually uploading the source maps
+        // Note: This is disabled in development mode.
+        if (
+            SENTRY_DSN &&
+            SENTRY_ORG &&
+            SENTRY_PROJECT &&
+            SENTRY_AUTH_TOKEN &&
+            COMMIT_SHA &&
+            NODE_ENV === 'production'
+        ) {
+            config.plugins.push(
+                new SentryWebpackPlugin({
+                    include: '.next',
+                    ignore: ['node_modules'],
+                    stripPrefix: ['webpack://_N_E/'],
+                    urlPrefix: `~${basePath}/_next`,
+                    release: COMMIT_SHA,
+                }),
+            )
+        }
 
-            // When all the Sentry configuration env variables are available/configured
-            // The Sentry webpack plugin gets pushed to the webpack plugins to build
-            // and upload the source maps to sentry.
-            // This is an alternative to manually uploading the source maps
-            // Note: This is disabled in development mode.
-            if (
-                SENTRY_DSN &&
-                SENTRY_ORG &&
-                SENTRY_PROJECT &&
-                SENTRY_AUTH_TOKEN &&
-                COMMIT_SHA &&
-                NODE_ENV === 'production'
-            ) {
-                config.plugins.push(
-                    new SentryWebpackPlugin({
-                        include: '.next',
-                        ignore: ['node_modules'],
-                        stripPrefix: ['webpack://_N_E/'],
-                        urlPrefix: `~${basePath}/_next`,
-                        release: COMMIT_SHA,
-                    }),
-                )
-            }
-            // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-            // config.plugins.push(new BundleAnalyzerPlugin({ token: process.env.BUNDLE_ANALYZER_TOKEN }))
+        
+        // Important: return the modified config
+        return config
+    },
+};
 
-            // Important: return the modified config
-            return config
-        },
-        basePath,
-    }
-);
+module.exports = withPlugins([
+    // add plugins here..
+    [withSourceMaps],
+    [withPreact]
+], nextConfig);
