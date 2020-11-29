6349e8b3bf5c2a9ec9c0245d0dbc48f0731cb66a
Silvio <silvio@mailbox.org>
2020-11-29 03:03:33 +0100

    comment out typescript, not configured yet

--- pages/about.js
+++ pages/about.js
@@ -1,29 +1,29 @@
-function buildName1(firstName: string, lastName = "Smith") {
-  return firstName + " " + lastName;
-}
-
-
-function buildName(firstName: string, lastName?: string) {
-  if (lastName) return firstName + " " + lastName;
-  else return firstName;
-}
-
-const myAdd1: (baseValue: number, increment?: number) => number = function (
-  x: number,
-  y: number = 1
-): number {
-  return x + y;
-};
-
-const myAdd2 = function (
-  x: number,
-  y: number = 1
-): number {
-  return x + y;
-};
-
-const myAdd3 = (x: number, y: number = 1): number => x + y;
-
+/* function buildName1(firstName: string, lastName = "Smith") {
+ *   return firstName + " " + lastName;
+ * }
+ * 
+ * 
+ * function buildName(firstName: string, lastName?: string) {
+ *   if (lastName) return firstName + " " + lastName;
+ *   else return firstName;
+ * }
+ * 
+ * const myAdd1: (baseValue: number, increment?: number) => number = function (
+ *   x: number,
+ *   y: number = 1
+ * ): number {
+ *   return x + y;
+ * };
+ * 
+ * const myAdd2 = function (
+ *   x: number,
+ *   y: number = 1
+ * ): number {
+ *   return x + y;
+ * };
+ * 
+ * const myAdd3 = (x: number, y: number = 1): number => x + y;
+ *  */
 export default function About () {
   return <div>About us</div>
 }
